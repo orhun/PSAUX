f9ae58715ec579fc7125445537eecc615b8215f2
KeyLo99 <keylo99official@gmail.com>
2019-06-24 19:06:13 +0200

    Update the about page

--- src/main/java/com/tht/psaux/AboutActivity.java
+++ src/main/java/com/tht/psaux/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.tht.psaux;
 
+import android.app.Dialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -8,109 +9,42 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-    private TextView txvAbout, txvKey, txvKeyEmail, txvTHT;
-    private ImageView imgKey, imgKeyTHT, imgKeyTw, imgKeyYt;
-    private RelativeLayout rlMain;
-    private Animation fadeIn;
-    private static String profileLink = "https://www.turkhackteam.org/members/763537.html";
-    private static String site = "https://www.turkhackteam.org";
+    private Context context;
+    private ViewGroup viewGroup;
+    private static String version = "1.0",
+            k3pwn = "http://www.k3pwn.me";
+    // ** //
+    private TextView txvK3pwn, txvProjectInfo;
     private void init(){
-        fadeIn = new AlphaAnimation(0, 1);
-        fadeIn.setInterpolator(new DecelerateInterpolator());
-        fadeIn.setDuration(500);
-        txvAbout = (TextView) findViewById(R.id.txvAbout);
-        txvAbout.startAnimation(fadeIn);
-        txvKey = (TextView) findViewById(R.id.txvKey);
-        txvKey.startAnimation(fadeIn);
-        txvKeyEmail = (TextView) findViewById(R.id.txvKeyEmail);
-        txvKeyEmail.startAnimation(fadeIn);
-        txvTHT = (TextView) findViewById(R.id.txvTHT);
-        txvTHT.startAnimation(fadeIn);
-        imgKey = (ImageView) findViewById(R.id.imgKey);
-        imgKey.startAnimation(fadeIn);
-        imgKeyTHT = (ImageView) findViewById(R.id.imgKeyTHT);
-        imgKeyTHT.startAnimation(fadeIn);
-        imgKeyTw = (ImageView) findViewById(R.id.imgKeyTw);
-        imgKeyTw.startAnimation(fadeIn);
-        imgKeyYt = (ImageView) findViewById(R.id.imgKeyYt);
-        imgKeyYt.startAnimation(fadeIn);
-        rlMain = (RelativeLayout) findViewById(R.id.activity_about);
+        txvK3pwn = viewGroup.findViewById(R.id.txvK3pwn);
+        txvProjectInfo = viewGroup.findViewById(R.id.txvProjectInfo);
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            version = info.versionName;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        txvProjectInfo.setText(context.getString(R.string.app_name) + " v" + version);
+        txvK3pwn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPage(k3pwn);
+            }
+        });
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        init();
         hideActionBar();
-        setVersionText();
-        txvAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String version = "";
-                try {
-                    PackageManager manager = getPackageManager();
-                    PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
-                    version = info.versionName;
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                Snackbar.make(rlMain, getResources().getString(R.string.app_name) + " v" + version, Snackbar.LENGTH_LONG).show();
-            }
-        });
-        txvKey.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(AboutActivity.this, "Coded by KeyLo99", Toast.LENGTH_LONG).show();
-            }
-        });
-        txvKeyEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                copyToClipBoard(txvKeyEmail.getText().toString());
-            }
-        });
-        imgKey.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openInBrowser(profileLink);
-            }
-        });
-        imgKeyTHT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openInBrowser(profileLink);
-            }
-        });
-        imgKeyTw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openTwitterIntent(getApplicationContext());
-            }
-        });
-        imgKeyYt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openYoutubeIntent(getApplicationContext());
-            }
-        });
-        txvTHT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openInBrowser(site);
-            }
-        });
+        init();
     }
     private void hideActionBar(){
         try {
@@ -124,54 +58,12 @@ public class AboutActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    private void setVersionText(){
-        String version = "1.0";
+    private void openPage(String url){
         try {
-            PackageManager manager = getPackageManager();
-            PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
-            version = info.versionName;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
         }catch (Exception e){
             e.printStackTrace();
         }
-        txvAbout.setText(getResources().getString(R.string.app_name) + " v" + version);
     }
-    private void copyToClipBoard(String data){
-        try{
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("PS-AUX", data);
-            clipboard.setPrimaryClip(clip);
-            Toast.makeText(getApplicationContext(), getString(R.string.copied), Toast.LENGTH_SHORT).show();
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-    }
-    private void openInBrowser(String uri) {
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            startActivity(intent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private void openTwitterIntent(Context context) {
-        try {
-            String id = "4745956696";
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name="+id));
-            startActivity(intent);
-        } catch (Exception e) {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/KeyLo_99"));
-            startActivity(intent);
-        }
-    }
-    private void openYoutubeIntent(Context context) {
-        try {
-            String id = "/channel/UCBOaNBR9j5FtdXEuGItqYqw/";
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://" + id));
-            startActivity(intent);
-        } catch (Exception e) {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCBOaNBR9j5FtdXEuGItqYqw"));
-            startActivity(intent);
-        }
-    }
-
 }
